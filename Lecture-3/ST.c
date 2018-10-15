86513c4dd4b131ba9ece6697add3c34a80a5e3f4
Yang Minshun <13592360+Abesthub@users.noreply.github.com>
2018-10-15 21:12:03 +0900

    Update

--- Lecture-3/ST.c
+++ Lecture-3/ST.c
@@ -128,10 +128,26 @@ link insertT(link h, Item item)
   return h;
 }
 
+link insertR(link h, Item item)
+{
+  Key v = key(item), t = key(h->item);
+  if (h == z)
+    return NEW(item, z, z, 1);
+  if (rand() < RAND_MAX / (h->N + 1))
+    return insertT(h, item);
+  if less
+    (v, t) h->l = insertR(h->l, item);
+  else
+    h->r = insertR(h->r, item);
+  (h->N)++;
+  return h;
+}
+
 void STinsert(Item item)
 {
   // head = insertT(head, item);
-  head = splay(head, item);
+  // head = splay(head, item);
+  head = insertR(head, item);
 }
 
 void sortR(link h, void (*visit)(Item))
--- Lecture-3/splayBST.c
+++ Lecture-3/splayBST.c
@@ -1,45 +1,63 @@
-link
-splay (link h, Item item)
+link splay(link h, Item item)
 {
-  Key v = key (item);
+  Key v = key(item);
   if (h == z)
-    return NEW (item, z, z, 1);
-  if (less (v, key (h->item)))
+    return NEW(item, z, z, 1);
+  if (less(v, key(h->item)))
+  {
+    if (h->l == z)
+      return NEW(item, z, h, h->N + 1);
+    if (less(v, key(h->l->item)))
     {
-      if (h->l == z)
-	return NEW (item, z, h, h->N + 1);
-      if (less (v, key (h->l->item)))
-	{
-	  h->l->l = splay (h->l->l, item);
-	  h = rotR (h);
-	}
-      else
-	{
-	  h->l->r = splay (h->l->r, item);
-	  h->l = rotL (h->l);
-	}
-      return rotR (h);
+      h->l->l = splay(h->l->l, item);
+      h = rotR(h);
     }
+    else
+    {
+      h->l->r = splay(h->l->r, item);
+      h->l = rotL(h->l);
+    }
+    return rotR(h);
+  }
   else
+  {
+    if (h->r == z)
+      return NEW(item, h, z, h->N + 1);
+    if (less(key(h->r->item), v))
     {
-      if (h->r == z)
-	return NEW (item, h, z, h->N + 1);
-      if (less (key (h->r->item), v))
-	{
-	  h->r->r = splay (h->r->r, item);
-	  h = rotL (h);
-	}
-      else
-	{
-	  h->r->l = splay (h->r->l, item);
-	  h->r = rotR (h->r);
-	}
-      return rotL (h);
+      h->r->r = splay(h->r->r, item);
+      h = rotL(h);
     }
+    else
+    {
+      h->r->l = splay(h->r->l, item);
+      h->r = rotR(h->r);
+    }
+    return rotL(h);
+  }
 }
 
-void
-STinsert (Item item)
+link insertT(link h, Item item)
 {
-  head = splay (head, item);
+  Key v = key(item);
+  if (h == z)
+    return NEW(item, z, z, 1); 
+  if (less(v, key(h->item))) 
+  {
+    h->l = insertT(h->l, item);
+    h = rotR(h);
+    (h->N)++;
+  }
+  else
+  {
+    h->r = insertT(h->r, item);
+    h = rotL(h);
+    (h->N)++;
+  }
+  return h;
+}
+
+void STinsert(Item item)
+{
+  head = splay(head, item);
 }
